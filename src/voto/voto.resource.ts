d40a6fcdfc2f5718ce4a3a15137cd5ad81744886
Rian-dpf <riandpf2002@gmail.com>
2024-06-22 13:43:39 -0300

    feat: Adding validation

--- src/voto/voto.resource.ts
+++ src/voto/voto.resource.ts
@@ -1,7 +1,12 @@
 import { OpcaoVoto } from "./voto.entity";
+import { IsNotEmpty, IsIn } from 'class-validator';
 
 export class RegistroVotoResource {
+
+    @IsNotEmpty({ message: 'CPF é um campo obrigatório.' })
     cpf: string;
 
+    @IsNotEmpty({ message: 'Opção de Voto é um campo obrigatório.' })
+    @IsIn([OpcaoVoto.NAO, OpcaoVoto.SIM], { message: "Opção de voto inválida, só coloque SIM ou NÃO." })
     opcaoVoto: OpcaoVoto;
 }
