f130ff8e39e042d8b716b71b559615472f095e7c
Rian-dpf <riandpf2002@gmail.com>
2024-06-22 09:45:06 -0300

    feat: Created voto and associado entity

--- src/voto/voto.service.ts
+++ src/voto/voto.service.ts
@@ -1,11 +1,13 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Voto } from './voto.entity';
+import { AssociadoService } from './associado/associado.service';
 
 @Injectable()
 export class VotoService {
     constructor(
         @Inject('VOTO_REPOSITORY')
-        private readonly votoRepository: Repository<Voto>
+        private readonly votoRepository: Repository<Voto>,
+        private readonly associadoService: AssociadoService
     ){}
 }
